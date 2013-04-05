0f02d1e2e02ab6cc22714a5e1ea3e4d3f03e1895
Calvin Metcalf <calvin.metcalf@state.ma.us>
2013-04-05 19:11:36 -0400

    now with communist

--- FractalLayer.js
+++ FractalLayer.js
@@ -1,22 +1,72 @@
+
+
+
 L.TileLayer.FractalLayer = L.TileLayer.Canvas.extend({
 	options: {
 		async: true
 	},
 	initialize: function () {
+        var workerFunc = function(data,cb) {
+    var id = data.id;
+    
+    var scale = Math.pow(2, data.z - 1);
+    var x0 = data.x / scale - 1;
+    var y0 = data.y / scale - 1;
+    
+    var d = 1/(256*scale);
+    var pixels = [];var MAX_ITER=500;
+    
+    for (var py = 0; py < 256; py++) {
+        for (var px = 0; px < 256; px++) {
         
+            var cx = x0 + px*d;
+            var cy = y0 + py*d;
+        
+            var x = 0, y = 0;
+            var xn, yn;
+            
+            var isOut = false;
+            
+            for (var iter = 0; iter < MAX_ITER; iter++) {
+                xn = x*x - y*y + cx;
+                yn = 2*x*y + cy;
+                
+                if (xn*xn + yn*yn > 4) {
+                    isOut = true;
+                    break;
+                }
+                
+                x = xn;
+                y = yn;
+            }
+            
+            var c = (iter/MAX_ITER)*255;
+            
+            pixels.push(0);
+            pixels.push(c);
+            pixels.push(0);
+            pixels.push(255);
+        }
+    }
+    
+    var array = new Uint8ClampedArray(pixels);
+    var buf = array.buffer;
+	cb({id: id, pixels: buf},[buf]);
+    
+}
 		var _this = this;
         
-		this._worker = new Worker('FractalRenderingWorker.js');
+		this._worker = communist(workerFunc)
         this._messages = [];
         this._waitingMessage = null;
         
-		this._worker.addEventListener('message', function(e) {
+		this.cb= function(data) {
             var msg = _this._waitingMessage;
-            //console.log('received', msg.id, _this._messages.length);
-            msg.callback(new Uint8ClampedArray(e.data.pixels), msg.date);
+            console.log(data);
+            msg.callback(new Uint8ClampedArray(data.pixels), msg.date);
             _this._waitingMessage = null;
             _this._postNextMessage();
-		}, false);
+		}
         
         this.on("tileunload", function(e) {
             var pos = e.tile._tileIndex,
@@ -37,9 +87,9 @@ L.TileLayer.FractalLayer = L.TileLayer.Canvas.extend({
             if (msg.cancelled) {
                 continue;
             }
-            
+            var _this = this;
             this._waitingMessage = msg;
-            this._worker.postMessage({id: msg.id, x: msg.x, y: msg.y, z: msg.z});
+            this._worker.data({id: msg.id, x: msg.x, y: msg.y, z: msg.z}).then(function(a){console.log("got it");_this.cb(a);},function(a){console.log("oh shit "+a)});
             break;
         }
     },
